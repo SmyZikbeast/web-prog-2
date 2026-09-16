d5f6005df6d30dc20a624db30a59d50da65f4526
SmyZ1k <zmkllkmk123pro@gmail.com>
2026-09-16 12:00:54 +0300

    1

--- js/checker.js
+++ js/checker.js
@@ -1,5 +1,5 @@
 export function get(x, y, r){
-    let url = "http://127.0.0.1:23452/fcgi-bin/server.jar?x="+x+"&y="+y+"&r="+r;
+    let url = "/fcgi-bin/server.jar?x="+x+"&y="+y+"&r="+r;
     fetch(url)
     .then((response) =>{
         return response.json();
